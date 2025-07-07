6f261b9b5a16a323b18f1e9e136950e027b4f29f
nilayotu <nilayotu_07@hotmail.com>
2025-07-07 23:45:20 +0300

    task-developerCompany has been completed

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Field;
 
